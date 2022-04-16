b6d3ff1603c5f8206dad1fac45f2b06397979ef2
dot-1q <tiagobarros99@ua.pt>
2022-04-16 21:59:59 +0100

    teste uart

--- ESP32/App3_ADC_UART/main/main.c
+++ ESP32/App3_ADC_UART/main/main.c
@@ -1,6 +1,25 @@
-#include <stdio.h>
+#include <driver/gpio.h>
+#include <driver/uart.h>
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
 
-void app_main(void)
-{
+int app_main() {
+    uart_config_t uart_config = {
+        .baud_rate = 115200,
+        .data_bits = UART_DATA_8_BITS,
+        .parity    = UART_PARITY_DISABLE,
+        .stop_bits = UART_STOP_BITS_1,
+        .flow_ctrl = UART_HW_FLOWCTRL_DISABLE
+    };
+    uart_driver_install(UART_NUM_1, 2048, 0, 0, NULL, 0);
+    uart_param_config(UART_NUM_1, &uart_config);
+    uart_set_pin(UART_NUM_1, 10, 9, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE);
+    // Main loop
+    int teste_var=0;
+    while(true) {
 
+        // Aqui iremos ler da dac e imprimir
+        uart_write_bytes(UART_NUM_1, teste_var, 32);
+        vTaskDelay(100 / portTICK_RATE_MS);
+    }
 }
